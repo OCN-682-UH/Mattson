9d91c5fc3cbac2b2f04aa6ccf1b015c26545b0c6
Nicholas Mattson <nsm6@hawaii.edu>
2026-09-14 21:02:23 -1000

    Here is my Histogram plot for Week 3 Homework

---
+++ Week_03/Scripts/Penguins_Script.R
@@ -0,0 +1,28 @@
+###This is the script for the ggplot 1 assignment###
+###Created by Nick Mattson###
+###Script created on 9-14-2026###
+########################################################
+
+###Load Libraries###
+library(palmerpenguins)
+library(tidyverse)
+glimpse(penguins)
+
+###Data Structure###
+head(penguins)
+
+###ggplot visual###
+ggplot(data=penguins,
+       mapping = aes(x = body_mass_g,
+                     fill = species))+
+  geom_histogram(bins = 30, alpha = 0.4, position = "dodge")+
+  labs(title = "Histogram of Penguin Body Mass by Species",
+       subtitle = "Comparison of Adelie, Gentoo, and Chinstrap Penguins",
+       x = "Body Mass (grams)", y = "Number of Penguins",
+       fill = "Species",
+       caption = "Palmer Station LTER/ palmerpenguins package")+
+  scale_fill_viridis_d()
+
+
+
+
